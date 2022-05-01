3d679d6701fa46df9797de39dfd3528a507f9754
Tommi Korhonen <tkorhonen13@gmail.com>
2022-05-01 14:16:46 +0300

    Update test

--- src/components/avatar/__test__/Avatar.test.tsx
+++ src/components/avatar/__test__/Avatar.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import Avatar from "../Avatar";
 
 const avatarProps = {
-  src: "http://localhost/Portfolio.png",
+  src: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
   h: 3,
   w: 3,
 };
