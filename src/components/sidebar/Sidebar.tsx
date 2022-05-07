666909fb2c18b49a3a19ad20cb552b2e543691ee
Tommi Korhonen <tkorhonen13@gmail.com>
2022-05-07 14:58:03 +0300

    change styling

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,35 +1,61 @@
 import React from "react";
 import Avatar from "../avatar/Avatar";
-import UserImg from "../../assets/user.png";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+//Hooks
 import { useLogout } from "../../hooks/useLogout";
-
-import { DocumentData } from "firebase/firestore";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useDocument } from "../../hooks/useDocument";
 
-interface ISidebarProps {
-  photoURL: string;
-}
+// Heroicons imports
+import { PlusCircleIcon } from "@heroicons/react/solid";
+import { TemplateIcon } from "@heroicons/react/solid";
+import { PencilAltIcon } from "@heroicons/react/solid";
+import { LogoutIcon } from "@heroicons/react/solid";
 
 const Sidebar = () => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
   const { document, error } = useDocument("users", user?.displayName);
   return (
-    <nav className="flex flex-col fixed items-center bg-violet-500 min-h-screen w-[200px] ">
+    <nav className="flex flex-col fixed items-center bg-violet-500 h-full w-[200px] ">
       <div className="p-4">
         {document && <Avatar src={document.photoURL} h={96} w={96} />}
       </div>
-      <div className="flex flex-col items-center mb-auto">
-        <h2 className="font-semibold ">Linkhub</h2>
-        <Link to="/create">Create</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/design">Design</Link>
+      <div className="flex flex-col items-center justify-between h-full">
+        <div className="flex flex-col gap-4">
+          <h2 className="font-bold text-center text-white mb-8">
+            Linkhub/{user?.displayName}
+          </h2>
+          <NavLink
+            to="/create"
+            className="flex items-center gap-2 py-2 px-4 rounded-md hover:bg-gray-500"
+          >
+            <PlusCircleIcon className="h-6 w-6 text-gray-200 " />
+            <span className="text-white font-medium">Create</span>
+          </NavLink>
+          <NavLink
+            to="/dashboard"
+            className="flex items-center gap-2 py-2 px-4 rounded-md hover:bg-gray-500"
+          >
+            <TemplateIcon className="h-6 w-6 text-gray-200" />
+            <span className="text-white font-medium">Dashboard</span>
+          </NavLink>
+          <NavLink
+            to="/design"
+            className="flex items-center gap-2 py-2 px-4 rounded-md hover:bg-gray-500"
+          >
+            <PencilAltIcon className="h-6 w-6 text-gray-200" />
+            <span className="text-white font-medium">Design</span>
+          </NavLink>
+        </div>
+        <button
+          onClick={logout}
+          className="flex items-center mb-4 gap-2 py-2 px-4 rounded-md hover:bg-gray-500"
+        >
+          <LogoutIcon className="h-6 w-6 text-gray-200" />
+          <span className="text-white font-medium">Logout</span>
+        </button>
       </div>
-      <h2 onClick={logout} className="cursor-pointer">
-        Logout
-      </h2>
     </nav>
   );
 };
