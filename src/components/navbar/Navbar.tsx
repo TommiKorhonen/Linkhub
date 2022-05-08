c0b4a7d59672fcf8326e7b2f2f0821911e4c4af2
Tommi Korhonen <tkorhonen13@gmail.com>
2022-05-08 19:47:28 +0300

    change navbar styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,14 +3,25 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex bg-red-50 w-full py-4 px-8">
-      <div className="mr-auto">
-        <h2 className="font-semibold">Linkhub</h2>
-      </div>
-      <div className="flex gap-4">
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign up</Link>
-      </div>
+    <nav className="w-full fixed top-0 bg-none py-4 px-8">
+      <ul className="flex items-center  mx-auto w-[1000px]">
+        <li className="mr-auto">
+          <Link to="/" className="font-semibold text-lg">
+            Linkhub
+          </Link>
+        </li>
+        <li className="flex gap-4">
+          <Link
+            to="/login"
+            className="border border-violet-500 border-solid text-violet-500 p-2 px-6"
+          >
+            Login
+          </Link>
+          <Link to="/signup" className="bg-violet-500 text-white p-2 px-6">
+            Sign up
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
