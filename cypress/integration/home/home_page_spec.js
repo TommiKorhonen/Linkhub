f22421640d7073a97201eab91822a3d54c86934c
Tommi Korhonen <tkorhonen13@gmail.com>
2022-05-09 22:05:20 +0300

    add cypress

---
+++ cypress/integration/home/home_page_spec.js
@@ -0,0 +1,15 @@
+const { cleanup } = require("@testing-library/react");
+
+beforeEach(() => {
+  cy.visit("http://localhost:3000");
+  cy.viewport(1920, 1080);
+});
+afterEach(() => {
+  cleanup();
+});
+
+describe("The Home Page", () => {
+  it("should be able to navigate to login", () => {
+    cy.contains(/login/i).click();
+  });
+});
