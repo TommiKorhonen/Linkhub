ec8b89cdf0f408798dec3111f1ade444fa110af9
Tommi Korhonen <tkorhonen13@gmail.com>
2022-05-15 20:53:29 +0300

    small update

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Preview, { ILink } from "../../components/preview/Preview";
 //Heroicons
 import { LinkIcon } from "@heroicons/react/solid";
@@ -13,6 +13,7 @@ const Dashboard = () => {
   const { updateDocument } = useFirestore("users");
   const { user } = useAuthContext();
   const { document, error } = useDocument("users", user?.displayName);
+  const [copyMessage, setCopyMessage] = useState("");
 
   const removeLink = async (linkId: string) => {
     if (linkId && user && user.displayName === document.id) {
@@ -21,7 +22,16 @@ const Dashboard = () => {
       });
     }
   };
-
+  const copyToClipboard = () => {
+    setCopyMessage("");
+    const url = navigator.clipboard.writeText(
+      `https://linkhub-3a46d.web.app/${user?.displayName}`
+    );
+    setCopyMessage("Url copied!");
+    return setTimeout(() => {
+      setCopyMessage("");
+    }, 3000);
+  };
   if (!document) {
     return (
       <div className="w-full h-screen flex items-center">
@@ -33,17 +43,16 @@ const Dashboard = () => {
     <main className="md:ml-72 sm:ml-44 grid grid-cols-1 lg:grid-cols-2 w-full min-h-screen">
       <section className="flex flex-col mx-auto p-4 sm:mx-0 sm:p-8">
         <h2 className="font-semibold text-3xl">Links</h2>
-        <button
-          className="w-80 flex items-center justify-center text-violet-500 bg-white py-1 border border-gray-200"
-          onClick={() =>
-            navigator.clipboard.writeText(
-              `https://linkhub-3a46d.web.app/${user?.displayName}`
-            )
-          }
-        >
-          <LinkIcon className="h-6 w-6" />
-          <span>{`linkhub/${user?.displayName}`}</span>
-        </button>
+        <div className="flex items-center gap-2">
+          <button
+            className="w-80 flex items-center justify-center text-violet-500 bg-white py-1 border border-gray-200"
+            onClick={() => copyToClipboard()}
+          >
+            <LinkIcon className="h-6 w-6" />
+            <span>{`linkhub/${user?.displayName}`}</span>
+          </button>
+          {copyMessage && <p className="bg-gray-200 p-2 ">{copyMessage}</p>}
+        </div>
         <ul>
           {document.links.length > 0 &&
             document.links.map((link: ILink) => (
