842aee2962a1c1134a67df0a2f149bc7ab4b0bd8
Tommi Korhonen <tkorhonen13@gmail.com>
2022-05-29 22:34:41 +0300

    remove import from global

--- src/components/styles/Global.ts
+++ src/components/styles/Global.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap');
+  
   * {
     box-sizing: border-box;
     margin: 0;
