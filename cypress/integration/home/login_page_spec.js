78e267d02e36976bd1180c473f4e8149eca3d0f9
Tommi Korhonen <tkorhonen13@gmail.com>
2022-06-01 21:59:02 +0300

    add login test

--- cypress/integration/home/login_page_spec.js
+++ cypress/integration/home/login_page_spec.js
@@ -3,15 +3,15 @@ const { cleanup } = require("@testing-library/react");
 const authUser = require("../../fixtures/example.json");
 beforeEach(() => {
   cy.viewport(1920, 1080);
+  cy.login();
 });
 
-describe("The Home Page", () => {
+describe("Login page", () => {
   it("should be able to login", () => {
-    const { email, password } = authUser;
     cy.visit("http://localhost:3000/login");
-    // cy.login(email, password);
-    // cy.contains("Links");
-    // cy.logout();
-    // cy.contains("Links").should("not.exist");
+    cy.contains(/logout/i);
+  });
+  afterEach(() => {
+    cy.logout();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,33 +1,36 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// import { initializeApp } from "firebase/app";
+// import { getAuth, signOut } from "firebase/auth";
+// import { signInWithEmailAndPassword } from "firebase/auth";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAdunmca4jJKI95WFFDlrksMTzXIevEWio",
+//   authDomain: "linkhub-3a46d.firebaseapp.com",
+//   projectId: "linkhub-3a46d",
+//   storageBucket: "linkhub-3a46d.appspot.com",
+//   messagingSenderId: "1057493039311",
+//   appId: "1:1057493039311:web:5259c4d95bae1746815fba",
+// };
+// initializeApp(firebaseConfig);
+// const auth = getAuth();
 
-import { initializeApp } from "firebase/app";
-import { getAuth, signOut } from "firebase/auth";
-import { signInWithEmailAndPassword } from "firebase/auth";
-const firebaseConfig = {
+// // eslint-disable-next-line no-undef
+// Cypress.Commands.add(
+//   "login",
+//   (email = "test-user@gmail.com", password = "test1234") => {
+//     return signInWithEmailAndPassword(auth, email, password);
+//   }
+// );
+// // eslint-disable-next-line no-undef
+// Cypress.Commands.add("logout", () => {
+//   return signOut(auth);
+// });
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/database";
+import "firebase/compat/firestore";
+import { attachCustomCommands } from "cypress-firebase";
+
+const fbConfig = {
   apiKey: "AIzaSyAdunmca4jJKI95WFFDlrksMTzXIevEWio",
   authDomain: "linkhub-3a46d.firebaseapp.com",
   projectId: "linkhub-3a46d",
@@ -35,17 +38,8 @@ const firebaseConfig = {
   messagingSenderId: "1057493039311",
   appId: "1:1057493039311:web:5259c4d95bae1746815fba",
 };
-initializeApp(firebaseConfig);
-const auth = getAuth();
+
+firebase.initializeApp(fbConfig);
 
 // eslint-disable-next-line no-undef
-Cypress.Commands.add(
-  "login",
-  (email = "test-user@gmail.com", password = "test1234") => {
-    return signInWithEmailAndPassword(auth, email, password);
-  }
-);
-// eslint-disable-next-line no-undef
-Cypress.Commands.add("logout", () => {
-  return signOut(auth);
-});
+attachCustomCommands({ Cypress, cy, firebase });
